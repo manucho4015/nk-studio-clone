5e4547be23be0ded17e4179e0d8960471d531aa1
Emmanuel Ochieng <eochieng16@gmail.com>
2025-07-01 15:01:11 +0300

    remove fontFamily global variable

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,6 @@ const config: Config = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
-            fontFamily: {
-                sans: ['Inter', 'sans-serif'],
-            },
             colors: {
                 background: '#0f0f0f',
                 primary: '#ffffff',
