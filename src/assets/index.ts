a2919b79705ced1606ac8098b0af0e95c42714ce
Emmanuel Ochieng <eochieng16@gmail.com>
2025-07-01 16:32:37 +0300

    import project images

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import SmartEdge from './smart-edge.png'
+import ManuchoMaps from './manucho-maps-dark.png'
+import EnigmaSite from './enigma-banner.png'
+
+export {
+    SmartEdge, ManuchoMaps, EnigmaSite
+}
