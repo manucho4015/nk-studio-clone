df5110c4d583440095f212ca4bca40798b6858b4
Emmanuel Ochieng <eochieng16@gmail.com>
2025-07-01 15:35:27 +0300

    integrate 'Header' component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 
 // components
 import HeroSection from "./components/HeroSection"
+import Header from "./components/Header"
 
 function App() {
 
   return (
     <div className="bg-background text-primary">
-      <HeroSection />
+      <Header />
+      <main>
+        <HeroSection />
+        <div className="h-screen bg-black" id="projects"></div>
+        <div className="h-screen bg-neutral-900" id="about"></div>
+        <div className="h-screen bg-black" id="contact"></div>
+      </main>
     </div>
   )
 }
